40d36ec03d6160e286281f51f9aa1dc04918b7e1
TotsawatAvat <32901299+TotsawatAvat@users.noreply.github.com>
2017-10-18 19:16:00 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CRUD SYSTEM</title>
+
+	<!-- bootstrap css -->
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<!-- datatables css -->
+	<link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css">
+
+</head>
+<body>
+
+	<dir class=<"container">
+		<div class="row">
+			<div class="col-md-12">
+				<h1>
+					My Firet Bootstrap
+				</h1>	
+			</div>
+			Hello word <br/>
+			My Name is BigBosMT
+			<button type="button" class="btn btn-primary" data-toggle"modal" data-target="#addmember">
+				Yer
+			</button>
+			<button type="button" class="btn btn-danger">No</button>
+		</div>	
+	</dir>
+
+	<div class="modal fade" role="dialog" id="addmember">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modai-header">
+					Modai-header
+				</div>
+				<div class="modal-boby">
+					Modai-boby
+				</div>
+				<div class="modal-footer">
+					Modai-footer
+				</div>
+			</div>
+		</div>	
+	</div>
+
+	<!-- jquery plugin -->
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<!-- bootstrap js -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<!-- datatables js -->
+	<script type="text/javascript" src="//cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+
+	
+
+</body>
+</html>
